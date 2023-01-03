d87e9c335f70e1023fa31960c11a082886ec3fd0
duizhang <duizhang627@163.com>
2023-01-04 02:09:01 +0800

    'xxx'

---
+++ nn-webfont/src/main/java/com/zhf/webfont/bo/UserLoginParam.java
@@ -0,0 +1,21 @@
+package com.zhf.webfont.bo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Author 10276
+ * @Date 2023/1/4 0:21
+ */
+@AllArgsConstructor
+@Data
+public class UserLoginParam {
+
+    @ApiModelProperty(value = "用户名",required = true)
+    private String username;
+
+    @ApiModelProperty(value = "密码",required = true)
+    private String password;
+
+}
---
+++ nn-webfont/src/main/java/com/zhf/webfont/service/impl/UserServiceImpl.java
@@ -0,0 +1,54 @@
+package com.zhf.webfont.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.Digester;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zhf.common.exception.Asserts;
+import com.zhf.webfont.bo.UserLoginParam;
+import com.zhf.webfont.mapper.UserMapper;
+import com.zhf.webfont.po.User;
+import com.zhf.webfont.service.UserService;
+import com.zhf.webfont.util.DigesterUtil;
+import com.zhf.webfont.util.JwtTokenUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Author 10276
+ * @Date 2023/1/4 0:16
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Resource
+    private UserMapper userMapper;
+    @Resource
+    private JwtTokenUtil jwtTokenUtil;
+    @Resource
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public String login(UserLoginParam userLoginParam) {
+        checkUserLoginParam(userLoginParam);
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("account",userLoginParam.getUsername());
+        List<User> users = userMapper.selectList(wrapper);
+        Asserts.failIsTrue(CollUtil.isEmpty(users),"账号或者密码错误");
+        User user = users.get(0);
+        String password = user.getPassword();
+        Asserts.failIsTrue(!passwordEncoder.matches(userLoginParam.getPassword(), password),"账号或者密码错误");
+        return jwtTokenUtil.generateToken(userLoginParam);
+    }
+
+    private void checkUserLoginParam(UserLoginParam userLoginParam) {
+        String username = userLoginParam.getUsername();
+        Asserts.failIsTrue(StrUtil.isEmpty(username),"请输入账号");
+
+        String inputPassword = userLoginParam.getPassword();
+        Asserts.failIsTrue(StrUtil.isEmpty(inputPassword),"请输入密码");
+    }
+}
